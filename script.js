e67290c67d0ec684e54b422c6bf27d7e2ebbe5f1
NikitaKhuspe1729 <nikitakhuspe1729@gmail.com>
2024-01-18 20:50:52 +0530

    add

---
+++ script.js
@@ -0,0 +1,29 @@
+let input = document.getElementById('input-box');
+let buttons = document.querySelectorAll('button');
+
+let string="";
+let arr = Array.from(buttons)
+arr.forEach(button =>{
+    button.addEventListener('click',(e) =>{
+
+        if(e.target.innerHTML == '='){
+            string=eval(string);  // eval function used for calculating 
+            input.value=string;
+        }
+        // for AC
+        else if(e.target.innerHTML == 'AC'){
+            string = "";
+            input.value =string;
+        }
+        // for DEL
+        else if(e.target.innerHTML == 'DEL'){
+            string =string.substring(0,string.length-1);
+            input.value = string;
+        }
+        // for all other, pass as it is
+        else{
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+    })
+})
